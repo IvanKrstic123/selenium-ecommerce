c8f38ceb1ec05e1ed3fed1725bd35ba6cb12d2c6
IvanKrstic123 <krstic8687@yahoo.com>
2021-12-30 14:49:53 +0100

    Add to Cart test implemented

---
+++ src/test/java/com/mystore/testcases/AddToCardPageTest.java
@@ -0,0 +1,43 @@
+package com.mystore.testcases;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.mystore.base.BaseClass;
+import com.mystore.pageobjects.AddToCartPage;
+import com.mystore.pageobjects.IndexPage;
+import com.mystore.pageobjects.SearchResultPage;
+
+public class AddToCardPageTest extends BaseClass {
+	
+	IndexPage indexPage;
+	SearchResultPage searchResultPage;
+	AddToCartPage addToCartPage;
+	
+	@BeforeMethod
+	public void setup() {
+		launchApp();
+	}
+
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
+	
+	@Test
+	public void addToCartTest() {
+		indexPage = new IndexPage();
+		searchResultPage =  indexPage.searchProduct("LG");
+		AddToCartPage addToCartPage = searchResultPage.clickOnProduct();
+		addToCartPage.enterQuantity("2");
+		addToCartPage.clickOnAddToCart();
+		
+		boolean result = addToCartPage.validateAddToCart();
+		
+		assertTrue(result);
+	}
+
+}
